a9e64b21ec1c24dd87ec02a43af842ea489217af
Dayse Abreu <abreudayse@gmail.com>
2018-10-09 23:06:12 -0300

    Retrieve data from firebase.

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,21 @@ import firebase from 'firebase';
 
 
 export default class Home extends Component {
+  state = {
+    employees: []
+  }
+
   componentWillMount() {
+
+    firebase.database().ref('employees')
+    .on('value', snapshot => {
+      this.setState({ employees: snapshot.val() });
+    });
 
   }
 
   render() {
+    console.log(this.state.employees);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Redux</Text>
