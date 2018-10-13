0c53ac4875e9fb8ae23fc32b190a4a235efde623
Dayse Abreu <abreudayse@gmail.com>
2018-10-13 09:01:47 -0300

    Add list of employees.

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,43 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, ListView} from 'react-native';
 import firebase from 'firebase';
+import _ from 'lodash';
 
+const ListItem = ( employee ) => {
+  console.log(employee);
+  const myEmployee = employee.employee;
+  return (
+    <View style={{backgroundColor: 'yellow'}}>
+      <Text>{myEmployee.name}</Text>
+    </View>
+  )
+}
 
 export default class Home extends Component {
   state = {
-    employees: []
+    employees: [],
+    dataSource: new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    })
   }
 
   componentWillMount() {
 
     firebase.database().ref('employees')
     .on('value', snapshot => {
-      this.setState({ employees: snapshot.val() });
+      const emp = _.map(snapshot.val(), (val, uid) => {
+        return { ...val, uid };
+      })
+      this.setState({ 
+        employees: emp,
+        dataSource: this.state.dataSource.cloneWithRows(emp)
+      });
     });
 
+  }
+
+  renderRow = (employee) => {
+    return <ListItem employee={employee}/>;
   }
 
   render() {
@@ -22,6 +45,11 @@ export default class Home extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Redux</Text>
+        <ListView
+          enableEmptySections
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     );
   }
@@ -30,8 +58,6 @@ export default class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
